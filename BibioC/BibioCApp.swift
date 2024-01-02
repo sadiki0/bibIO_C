f5fd660b5af7694d43757dfc5ac13544c12acfe0
MARK SADIKI <mngishu@icloud.com>
2024-01-02 14:35:15 -0600

    Initial Commit

---
+++ BibioC/BibioCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibioCApp.swift
+//  BibioC
+//
+//  Created by SADIKI on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BibioCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
